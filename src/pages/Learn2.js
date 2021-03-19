79e757f5a1b2de7de8259074e269eb3d193d78d6
mooroom <61869246+mooroom@users.noreply.github.com>
2021-03-19 12:45:20 +0900

    Update Learn2.js

--- src/pages/Learn2.js
+++ src/pages/Learn2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 // packages
 import { Link } from "react-router-dom";
@@ -10,9 +10,26 @@ import BodyBackground from "../components/BodyBackground";
 import Button from "../components/Button";
 import Container from "../components/Container";
 import BottomBox from "../components/BottomBox";
+import Alert from "../components/Alert";
+
 
 function Learn2(props) {
-  const linkHandler = () => "/prepare";
+  const [ended, setEnded] = useState(false);
+  const [show, setShow] = useState(false);
+  const [link, setLink] = useState("#")
+
+  const linkHandler = () => {
+    if (!ended) {
+      setShow(true);
+      setTimeout(() => setShow(false), 500);
+    } 
+  };
+
+  useEffect(() => {
+    if (ended) {
+      setLink("/prepare")
+    }
+  }, [ended])
 
   return (
     <>
@@ -23,14 +40,15 @@ function Learn2(props) {
           url="https://www.youtube.com/embed/h0Ls3unm-nA"
           width="100%"
           height="auto"
+          onEnded={() => setEnded(true)}
         />
       </Container>
-
       <BottomBox>
-        <Link to={linkHandler}>
-          <Button width="100%">다음으로 넘어가기</Button>
+        <Link to={link}>
+          <Button width="100%" onClick={linkHandler}>다음으로 넘어가기</Button>
         </Link>
       </BottomBox>
+      <Alert text="동영상을 모두 보고 난 후 다음 페이지로 갈 수 있어!" show={show}/>
     </>
   );
 }
